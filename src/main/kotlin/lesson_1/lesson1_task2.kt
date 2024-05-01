0486e207a4e9ea0dac5a967db6272977386b872c
ilham <im615143@gmail.com>
2024-05-01 20:20:17 +0400

    KS-1-2 add employee count value

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -2,4 +2,17 @@ package org.example.lesson_1
 
 fun main() {
 
+    val orderCount: Int = 200
+    val textOfGratitude : String = "Спасибо за покупку! Ваш заказ обрабатывается..."
+    var employeeCount: Int = 200 //количество работников. Переменная var, так как она в будущем будет меняться.
+
+    //вывод данных переменных
+    println("$textOfGratitude\nКоличество заказов: $orderCount")
+
+    //вывод количества работников
+//    println("Количество работников: $employeeCount")
+
+    //Один работник уволился. Вывод нового значения. Префиксная декрементация.
+    println("Обновленное количество работников: ${--employeeCount}")
+
 }
