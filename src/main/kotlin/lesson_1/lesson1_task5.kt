8bb41b1a60a7cbb1d0e4184227b9f4bc4883b6a9
ilham <im615143@gmail.com>
2024-05-01 21:09:09 +0400

    KS-1-5 not yet complete

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,5 +2,14 @@ package org.example.lesson_1
 
 fun main() {
 
-    val secondsInSpace: Int
+    val secondsInSpace: Int = 6480;
+
+    val minute: Int = secondsInSpace%3
+    val hour: Int = secondsInSpace%360
+    val remainder: Int = secondsInSpace%60
+
+    println(hour)
+    println(minute)
+    println(remainder)
+
 }
