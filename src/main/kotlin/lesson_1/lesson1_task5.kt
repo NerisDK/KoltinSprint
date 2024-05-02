00e112787eede409160f33ed1aa480ad9ed8ee9d
ilham <im615143@gmail.com>
2024-05-02 09:59:34 +0400

    KS-1-5 complete

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,14 +2,18 @@ package org.example.lesson_1
 
 fun main() {
 
-    val secondsInSpace: Int = 6480;
+    val secondsInSpace: Int = 6483 //Я добавил 3 секунды к значению из прошлой задачи
+    var remainder: Int = secondsInSpace //Остаток от секунд. Var, так как будет меняться по ходу задачи
 
-    val minute: Int = secondsInSpace%3
-    val hour: Int = secondsInSpace%360
-    val remainder: Int = secondsInSpace%60
+    //Вычисляем сколько часов в значении secondsInSpace, вычисляем остаток путем вычитания
+    val hour: Int = secondsInSpace/3600
+    remainder = secondsInSpace - (hour*3600)
 
-    println(hour)
-    println(minute)
-    println(remainder)
+    //Аналогично для минут
+    val minute: Int = remainder/60
+    remainder = remainder - (minute*60)
 
+    //Вывод значений
+    print("Время в космосе: ") //Print, чтобы не было переноса на новую строку
+    print(String.format("[%02d:%02d:%02d]", hour, minute, remainder))
 }
