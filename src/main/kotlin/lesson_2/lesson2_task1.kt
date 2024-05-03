c8a30c166017bdcb5ccd7837d38ef1923fc385e9
ilham <im615143@gmail.com>
2024-05-03 12:57:11 +0400

    KS-2-1-fix-changes

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -6,7 +6,16 @@ fun main() {
     val point2: Double = 4.0
     val point3: Double = 3.0
     val point4: Double = 5.0
-    val averagePoint: Double = (point1+point2+point3+point4)/4
+    val pointCount: Int = countOf(point1, point2, point3, point4)
+    val averagePoint: Double = (point1 + point2 + point3 + point4) / pointCount
 
-    println(averagePoint)
+    println("Среднее арифметическое: $averagePoint")
+}
+
+fun countOf(vararg objects: Any): Int {
+    var count: Int = 0
+    for(obj in objects) {
+        count++
+    }
+    return count
 }
