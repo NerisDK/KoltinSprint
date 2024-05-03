bc586bd81e5984e72446995ed2db21e5b70e733c
ilham <im615143@gmail.com>
2024-05-03 13:03:57 +0400

    KS-2-3-fix-changes

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -4,15 +4,15 @@ fun main() {
 
     val hourOfStart: Int = 9
     val minuteOfStart: Int = 39
-    val startTimeInMinutes: Int = (hourOfStart*60) + minuteOfStart
+    val minuteInOneHour: Int = 60 //min
     val travelTimeInMinutes: Int = 457
+    val startTimeInMinutes: Int = (hourOfStart * minuteInOneHour) + minuteOfStart
     var endTimeInMinutes: Int = startTimeInMinutes + travelTimeInMinutes
 
-    var hourOfEnd: Int = endTimeInMinutes/60
-    endTimeInMinutes -= hourOfEnd*60
+    val hourOfEnd: Int = endTimeInMinutes / minuteInOneHour
+    endTimeInMinutes -= hourOfEnd * minuteInOneHour
     val minuteOfEnd: Int = endTimeInMinutes
 
-    print("Время приезда: ")
-    println("[$hourOfEnd:$minuteOfEnd]")
+    print("Время приезда: [$hourOfEnd:$minuteOfEnd]")
 
 }
