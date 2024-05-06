d26102f20d1266fbac7f6df0891f8fc7ced7a99a
ilham <im615143@gmail.com>
2024-05-06 16:35:04 +0400

    KS-3-2-complete

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val oldName: String = "Татьяна"
+    val oldSurname: String = "Андреева"
+    val oldPatronymic: String = "Сергеевна"
+    val oldAge: Int = 20
+
+    val newName: String = "Татьяна"
+    val newSurname: String = "Сидорова"
+    val newPatronymic: String = "Сергеевна"
+    val newAge: Int = 22
+
+    println("Старые данные:  [$oldSurname $oldName $oldPatronymic, $oldAge]")
+    println("Новые данные: [$newSurname $newName $newPatronymic, $newAge]")
+
+}
