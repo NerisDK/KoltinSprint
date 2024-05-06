8bc5794576ff540d15416d0713988cef86965a7b
ilham <im615143@gmail.com>
2024-05-06 19:43:23 +0400

    KS-4-1-complete

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,2 +1,12 @@
 package org.example.lesson_4
 
+fun main() {
+
+    val tableCount: Int = 13
+    val reservedTablesForToday: Int = 13
+    val reservedTablesForTomorrow: Int = 9
+
+    println("Доступность столиков на сегодня: ${if (reservedTablesForToday < tableCount) true else false }")
+    println("Доступность столиков на завтра: ${if (reservedTablesForTomorrow < tableCount) true else false }")
+
+}
