175c6258a443b35bf93c96fab61ca57022442875
ilham <im615143@gmail.com>
2024-05-06 17:14:45 +0400

    KS-3-5-complete

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -4,4 +4,17 @@ fun main() {
 
     val inputMoveInfo: String = "D2-D4;0"
 
+    val moveIntoArray: CharArray = inputMoveInfo.toCharArray()
+
+    val startPositionLetter: Char = moveIntoArray[0]
+    val startPositionNumber: Int = moveIntoArray[1].code - '0'.code
+    val endPositionLetter: Char = moveIntoArray[3]
+    val endPositionNumber: Int = moveIntoArray[4].code - '0'.code
+    val moveNumber: Int = moveIntoArray[6].code - '0'.code
+
+    println(startPositionLetter)
+    println(startPositionNumber)
+    println(endPositionLetter)
+    println(endPositionNumber)
+    println(moveNumber)
 }
