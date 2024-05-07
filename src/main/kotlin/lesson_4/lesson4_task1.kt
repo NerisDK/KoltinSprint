f967dfc89573de8a8e536bfe042a67c4f663ce4b
ilham <im615143@gmail.com>
2024-05-07 21:21:33 +0400

    KS-4-1-fix-changes

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -2,11 +2,11 @@ package org.example.lesson_4
 
 fun main() {
 
-    val tableCount: Int = 13
+    val tableCount: Int = 13 //постоянное количество столов
     val reservedTablesForToday: Int = 13
     val reservedTablesForTomorrow: Int = 9
 
-    println("Доступность столиков на сегодня: ${if (reservedTablesForToday < tableCount) true else false }")
-    println("Доступность столиков на завтра: ${if (reservedTablesForTomorrow < tableCount) true else false }")
+    println("Доступность столиков на сегодня: ${reservedTablesForToday < tableCount}")
+    println("Доступность столиков на завтра: ${reservedTablesForTomorrow < tableCount}")
 
 }
