b870dfabfe7452099a3254301e07d443ebdd093a
ilham <im615143@gmail.com>
2024-05-08 11:47:35 +0400

    KS-4-2-fix-changes

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -2,16 +2,25 @@ package org.example.lesson_4
 
 fun main() {
 
-    val minWeight: Int = 35 //кг
-    val maxWeight: Int = 100 //кг
-    val maxVolume: Int = 100 //литров
 
     var givenWeight: Int = 20 //кг
     var givenVolume: Int = 80 //литров
-    println("Груз с весом $givenWeight кг и объемом $givenVolume литров соответствует категории 'Average': ${(givenWeight <= maxWeight && givenWeight > minWeight && givenVolume < maxVolume)}")
+    var isAverage: Boolean = givenWeight <= MAX_WEIGHT && givenWeight > MIN_WEIGHT && givenVolume <= MAX_VOLUME
+    println(
+        "Груз с весом $givenWeight кг и объемом $givenVolume литров соответствует категории 'Average': " +
+                "$isAverage"
+    )
 
     givenWeight = 50 //кг
     givenVolume = 100 //литров
-    println("Груз с весом $givenWeight кг и объемом $givenVolume литров соответствует категории 'Average': ${(givenWeight <= maxWeight && givenWeight > minWeight && givenVolume < maxVolume)}")
+    isAverage = givenWeight <= MAX_WEIGHT && givenWeight > MIN_WEIGHT && givenVolume <= MAX_VOLUME
+    println(
+        "Груз с весом $givenWeight кг и объемом $givenVolume литров соответствует категории 'Average': " +
+                "$isAverage"
+    )
 
 }
+
+const val MIN_WEIGHT: Int = 35 //кг
+const val MAX_WEIGHT: Int = 100 //кг
+const val MAX_VOLUME: Int = 100 //литров
